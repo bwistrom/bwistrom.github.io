7814255438869e9d3227811f0f0205ffbbdf8fd8
bwistrom <003wistrom@gmail.com>
2025-03-03 16:18:37 +0100

    Inlämningsuppgift HTML & CSS
    Björn Wiström ~ Portfolio

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".cta-button");
+    
+    button.addEventListener("click", function() {
+        alert("Fun fact: Den första webbutvecklaren i världen var Tim Berners-Lee, som skapade den allra första webbplatsen år 1991. Webbplatsen fanns på CERN och handlade om hur man använde World Wide Web. Den finns fortfarande kvar och kan besökas här: info.cern.ch 🚀🌍");
+    });
+});
