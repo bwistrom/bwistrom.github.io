c43c5dc1fcf149082a463ec620967e9f281a7f35
bwistrom <003wistrom@gmail.com>
2025-03-30 19:22:03 +0200

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.querySelector(".cta-button");
-    
-    button.addEventListener("click", function() {
-        alert("Fun fact: Den första webbutvecklaren i världen var Tim Berners-Lee, som skapade den allra första webbplatsen år 1991. Webbplatsen fanns på CERN och handlade om hur man använde World Wide Web. Den finns fortfarande kvar och kan besökas här: info.cern.ch 🚀🌍");
-    });
-});
